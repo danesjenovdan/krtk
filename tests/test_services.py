4ecfde121911aef753043464b509f908d372d16b
mzgajner <mato@zgajner.com>
2024-01-19 19:44:05 +0100

    Move existing services tests into same file

--- tests/test_services.py
+++ tests/test_services.py
@@ -68,3 +68,21 @@ class TestPublicServices(TestCase):
         self.assertFalse(link_with_alias_exists("c"))
 
 
+class TestGenerateNextAlias(TestCase):
+    def test_simple_examples(self):
+        self.assertEqual(_generate_next_alias("a"), "b")
+        self.assertEqual(_generate_next_alias("h"), "i")
+        self.assertEqual(_generate_next_alias("y"), "z")
+        self.assertEqual(_generate_next_alias("aaab"), "aaac")
+        self.assertEqual(_generate_next_alias("zzzy"), "zzzz")
+        self.assertEqual(_generate_next_alias("lulek"), "lulel")
+
+    def test_all_zs(self):
+        self.assertEqual(_generate_next_alias("z"), "aa")
+        self.assertEqual(_generate_next_alias("zzz"), "aaaa")
+        self.assertEqual(_generate_next_alias("zzzzzzzzzz"), "aaaaaaaaaaa")
+
+    def test_overflow(self):
+        self.assertEqual(_generate_next_alias("az"), "ba")
+        self.assertEqual(_generate_next_alias("azz"), "baa")
+        self.assertEqual(_generate_next_alias("yzzzzz"), "zaaaaa")
